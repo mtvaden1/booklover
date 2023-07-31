d319c3966df03b3c7784bc949d0e56b0bc676517
mtvaden1 <mtv2eva@virginia.edu>
2023-07-31 11:09:29 -0400

    remove demo from repo

--- book_lover_demo.py
+++
@@ -1,7 +0,0 @@
-from booklover import booklover
-import pandas as pd
-
-test = booklover.BookLover("Han Solo", "hsolo@millenniumfalcon.com", "scifi")
-test.add_book("War of the Worlds", 4)
-
-print("The number of books in your list is: ", test.num_books)
